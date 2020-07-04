ebea2df574c5488affeb396c53be376aa0ba9163
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-04 13:06:11 +0530

    Update library (1) (1).sql

--- library (1) (1).sql
+++ library (1) (1).sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 11, 2019 at 09:42 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
